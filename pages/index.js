d5952d5faa163bae6945b91ffce9efae003378b0
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-02-28 15:52:11 +0800

    added mongoose to use mongoDB database

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Link from "next/link";
 import firebase from "../firebase/clientApp";
 import { useUser } from "../context/userContext";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
   // Our custom hook to get context values
   const { loadingUser, user } = useUser();
   const router = useRouter();
+  const [todoText, setTodoText] = useState("");
 
   function handleLogout() {
     firebase.auth().signOut();
@@ -22,12 +23,20 @@ export default function Home() {
     return null;
   }
 
+  function addTodoToDatabase() {}
+
   return (
     <div>
       <main>
         <Link href={`/dashboard/${user.uid}`}> Go to User dashboard </Link>
-        <button onClick={handleLogout}>logout</button>
+        <input
+          type="text"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button onClick={addTodoToDatabase}></button>
       </main>
+      <button onClick={handleLogout}>logout</button>
     </div>
   );
 }
