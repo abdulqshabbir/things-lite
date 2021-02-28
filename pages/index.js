dfe8b27d6f4154ccf4d077d5b4889594e048ad84
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-02-28 13:07:01 +0800

    user is null error fixed.  Now when the user logs out, the useEffect hook fires to redirect the user to the signup page.  Also, the index page renders nothing to ensure no errors occur.

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,40 @@
 import Link from "next/link";
+import firebase from "../firebase/clientApp";
 import { useUser } from "../context/userContext";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
   // Our custom hook to get context values
   const { loadingUser, user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("change to user or loadingUser");
+    if (user === null) {
+      router.push("/signup");
+    }
+    return () => {};
+  }, [user, loadingUser]);
+
+  function handleLogout() {
+    firebase.auth().signOut();
+  }
 
   if (loadingUser) {
     return <div>Loading...</div>;
   }
 
   if (user === null) {
-    return <div>Error...</div>;
+    router.push("/signup");
+    return null;
   }
 
   return (
     <div>
       <main>
         <Link href={`/dashboard/${user.uid}`}> Go to User dashboard </Link>
+        <button onClick={handleLogout}>logout</button>
       </main>
     </div>
   );
