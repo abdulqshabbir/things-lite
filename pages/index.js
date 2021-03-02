94aa74a6696e4bd882771cc78a44616aa906aa72
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-03-03 06:24:36 +0800

    added things-lite files into project

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useUser } from "../context/userContext";
+import dbConnect from "../database/dbConnect";
+import firebase from "../firebase/clientApp";
+import Todo from "../models/Todo";
 
-export default function Home() {
+export default function Home({ todos }) {
+  // Our custom hook to get context values
+  const { loadingUser, user } = useUser();
+  const router = useRouter();
+  const [todoText, setTodoText] = useState("");
+
+  function handleLogout() {
+    firebase.auth().signOut();
+  }
+
+  if (loadingUser) {
+    return <div>Loading...</div>;
+  }
+
+  if (user === null) {
+    router.push("/signup");
+    return null;
+  }
+
+  function addTodoToDatabase() {}
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+    <div>
+      <main>
+        <Link href={`/dashboard/${user.uid}`}> Go to User dashboard </Link>
+        <input
+          type="text"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button onClick={addTodoToDatabase}></button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {todos.map((t) => (
+        <li key={t._id} id={t._id}>
+          {t.title}
+        </li>
+      ))}
+      <button onClick={handleLogout}>logout</button>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  await dbConnect();
+
+  // find all todos and turn them to plain javascript obejcts
+  const todoDocs = await Todo.find({});
+
+  const todos = todoDocs.map((doc) => {
+    // convert to serializable object
+    const todo = doc.toObject();
+    todo._id = todo._id.toString();
+    return todo;
+  });
+
+  return {
+    props: {
+      todos,
+    },
+  };
 }
