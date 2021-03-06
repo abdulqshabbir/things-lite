16e536bb35f115ee0cf38e44a4c6ca6a1309c121
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-03-06 19:19:59 +0800

    deleted useTodos and added isomorphic fetch to dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,73 @@
-import useTodos from "../fetchData/useTodos";
+import fetch from "isomorphic-fetch";
+import { BASE_API_URL } from "../utils/config";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Home() {
-  const { todos, isLoading, isError } = useTodos();
+export default function Home({ notes }) {
+  const [newTodo, setNewTodo] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
 
-  if (isLoading) return <div>is loading...</div>;
+  async function createNewTodo(e) {
+    e.preventDefault();
+    setNewTodo("");
+    setIsSubmitting(true);
+    await fetch(`/api/todos`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: newTodo,
+      }),
+    }).catch((e) => console.log(e));
+    setIsSubmitting(false);
+    router.push("/");
+  }
 
-  if (isError) return <div>error...</div>;
+  if (!notes.success) {
+    return (
+      <div>
+        <p>Something went wrong with fetching the notes...</p>
+        <input
+          type="text"
+          placeholder="Type new todo here..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={(e) => createNewTodo(e)}>Create New Todo</button>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          {notes.data.map((n) => (
+            <li key={n._id}>{n.title}</li>
+          ))}
+        </div>
+        <div>
+          {isSubmitting ? "Loading" : null}
+          <input
+            type="text"
+            placeholder="Type new todo here..."
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={(e) => createNewTodo(e)}>Create New Todo</button>
+        </div>
+      </>
+    );
+  }
+}
 
-  return (
-    <div>
-      {todos.data.map((t) => (
-        <li key={t._id}>{t.title}</li>
-      ))}
-    </div>
-  );
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/todos`);
+  const notes = await res.json();
+  return {
+    props: {
+      notes: notes,
+    },
+  };
 }
