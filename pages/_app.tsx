a494606bee1e8416117629519aaee9223287bac4
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-03-07 10:29:13 +0800

    converted some easy files to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import UserProvider from "../context/userContext";
 
 // Custom App to wrap it with context provider
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps}: any) {
   return (
     <UserProvider>
       <Component {...pageProps} />
